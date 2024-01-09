0c2293a7137263b5a944b02dc1770adf5eb5b51c
Liubre82 <liubre82@students.rowan.edu>
2024-01-09 04:17:36 -0500

    Setting up Project

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
 
+        char[][] board = {{' ', ' ', ' '},
+                          {' ', ' ', ' '},
+                          {' ', ' ', ' '}};
+
+        char[][] boardPosition = {{'1', '2', '3'},
+                                  {'4', '5', '6'},
+                                  {'7', '8', '9'}};
+
+        printBoard(board, boardPosition);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Where would you like to play? ");
+        String userInput = scanner.nextLine();
+        System.out.println(userInput);
+    }
+
+    //Printing out our board in the terminal.
+    private static void printBoard(char[][] board, char[][] boardPosition) {
+        System.out.print("Game Board" + "   " + "Board Position");
+        System.out.println();
+        System.out.print("  " + board[0][0] + "|" + board[0][1] + "|" + board[0][2] + "        ");
+        System.out.print("  " + boardPosition[0][0] + "|" + boardPosition[0][1] + "|" + boardPosition[0][2]);
+        System.out.println();
+        System.out.print("  -+-+-" + "        ");
+        System.out.print("  -+-+-");
+        System.out.println();
+        System.out.print("  " + board[1][0] + "|" + board[1][1] + "|" + board[1][2] + "        ");
+        System.out.print("  " + boardPosition[1][0] + "|" + boardPosition[1][1] + "|" + boardPosition[1][2]);
+        System.out.println();
+        System.out.print("  -+-+-" + "        ");
+        System.out.print("  -+-+-");
+        System.out.println();
+        System.out.print("  " + board[2][0] + "|" + board[2][1] + "|" + board[2][2] + "        ");
+        System.out.print("  " + boardPosition[2][0] + "|" + boardPosition[2][1] + "|" + boardPosition[2][2]);
+        System.out.println();
     }
 }
